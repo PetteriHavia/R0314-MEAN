527492c25b609a32658127c0b06bc8a64c9ea073
Petteri Havia <1803106@laurea.fi>
2020-02-27 11:58:48 +0200

    WS3 DONE

--- WS3/anotherapi.js
+++ WS3/anotherapi.js
@@ -2,7 +2,7 @@
 var fs = require("fs");
 var http = require("http");
 var axios = require('axios');
-var pokemon = require("pokemontcgsdk"); //npm install pokemontcgsdk
+const pokemon = require("pokemontcgsdk"); //npm install pokemontcgsdk
 var json;
 
 const promise = axios
@@ -11,26 +11,61 @@ const promise = axios
     const data = response.data;
     json = data;
     console.log(json); // log JSON information
-});
+    var html = parseData(json); // Call function
 
 http.createServer(function(request, response) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write("<h1 align='center'>Pokemon Card Collection<h1>");
+    response.write( html );
+    response.end();
+    console.log(html);
+
+    //  response.end(); //end the response
+  })
+  .listen(8081); //the server object listens on port 8080
+});
+
+//Parse function
+function parseData(data) {
+
+    var html = "<table border='0' align='center'>";
+    for (var i = 0; i < data.cards.length; i++) {  //Loop and show information
+    html+= "<tr>";
+    html += "<td><img src='" + data.cards[i].imageUrl + "'></td></td>";
+    html+= "<td padding-left='10px'>" + "NAME: " + data.cards[i].id + "<br>"
+    + "SERIES: " + data.cards[i].rarity + "</td>";
+    
+    html += "</tr>";
+    }
+    html += "</table>";
+    console.log(html);
+    return html;
+}
+
+/*
+http.createServer(function(request, response) {
     response.writeHead(200, {'Content-Type': 'text/html'}); 
-    response.write("<html><body>");
+
+    response.write(html)
+
+    /*response.write("<html><body>");
     response.write("<h1 align ='center'>CARD COLLECTION</h1>");
-    response.write("<table border ='solid 2px black' align='center'>");
+
+    response.write("<table border ='solid 3px black' align='center'>");
 
     for (var i=0; i < json.Search.length; i++) {  //Loop and show information
         response.write("<tr>");
-        response.write(("</td><td><img style='width:70%' src=" + json[i].imageUrl +">"
-        + "</td><td>" + "NAME: " + json[i].name + "</td><td>"
-        + "NUMBER: " + json[i].set + "</td>"));
+        response.write(("</td><td><img style='width:70%' src=" + json.Search[i].imageUrl +">"
+        + "</td><td>" + "NAME: " + json.Search[i].name + "</td><td>"
+        + "SET: " + jsonSearch[i].set + "</td>"));
         response.write("</tr>");
     };
     response.write("</table>");
-    response.write("</body></html>");//jotain
+    response.write("</body></html>");
+
 
 response.end();
 }).listen(8081);
 console.log("Server running at http://127.0.0.1:8081/");
 
-
+*/
