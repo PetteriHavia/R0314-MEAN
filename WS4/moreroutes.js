d5cec263e564c4b8e590b348b066ba9eacb966b5
PetteriH <petsku15@hotmail.fi>
2020-03-01 16:41:57 +0200

    WS4 stuff

--- WS4/moreroutes.js
+++ WS4/moreroutes.js
@@ -1,15 +1,36 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 app.use(express.static('/public/demoite/'));
 
 app.get('/', function (req, res) {
-    res.sendfile("__dirname + /public/index.html");
+    res.sendFile(__dirname + '/public/demosite/index.html');
 });
 
 app.get('/list', function (req, res) {
-    res.send("Listing data from a file!");
+    res.sendFile(__dirname + '/exampledata.txt');
 });
+
+app.get('/jsondata', function(req, res) {
+    var data = require('./exampledata2.json');
+    res.json(data);
+})
+
+app.get('/details', function (req, res) {
+    var data = require('./exampledata2.json');
+
+    var result = '<table border="1">';
+
+    for(var i=0; i < data.length; i++) {
+        results +=
+        '<tr>'+
+        '<td>' + data[i].Name +'</td>'+
+        '<td>' + data[i].Email +'</td>'+
+        '</tr>';
+    }
+    res.send(results);
+})
 
 app.get('/add', function(req, res) { 
     res.send('Lets try to add some data to file');
