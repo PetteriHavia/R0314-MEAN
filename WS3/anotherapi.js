9f4aa464d9c2ef40cdb217e98337c7b9dd0e1c3d
PetteriH <petsku15@hotmail.fi>
2020-03-01 15:00:32 +0200

    WS3 DONE

--- WS3/anotherapi.js
+++ WS3/anotherapi.js
@@ -15,12 +15,10 @@ const promise = axios
 
 http.createServer(function(request, response) {
     response.writeHead(200, { "Content-Type": "text/html" });
-    response.write("<h1 align='center'>Pokemon Card Collection<h1>");
+    response.write("<h1 align='center'>POKEMON CARD COLLECTION<h1>");
     response.write( html );
     response.end();
     console.log(html);
-
-    //  response.end(); //end the response
   })
   .listen(8081); //the server object listens on port 8080
 });
@@ -31,9 +29,11 @@ function parseData(data) {
     var html = "<table border='0' align='center'>";
     for (var i = 0; i < data.cards.length; i++) {  //Loop and show information
     html+= "<tr>";
-    html += "<td><img src='" + data.cards[i].imageUrl + "'></td></td>";
-    html+= "<td padding-left='10px'>" + "NAME: " + data.cards[i].id + "<br>"
-    + "SERIES: " + data.cards[i].rarity + "</td>";
+    html += "<td><img src='" + data.cards[i].imageUrl + "'></td>";
+    html+= "<td>" + "NAME: " + data.cards[i].name + "<br>"
+    + "TYPE: " + data.cards[i].types + "</br>"
+    + "SET: " + data.cards[i].set + "</br>"
+    + "RARITY: " + data.cards[i].rarity + "</td>";
     
     html += "</tr>";
     }
@@ -41,31 +41,3 @@ function parseData(data) {
     console.log(html);
     return html;
 }
-
-/*
-http.createServer(function(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'}); 
-
-    response.write(html)
-
-    /*response.write("<html><body>");
-    response.write("<h1 align ='center'>CARD COLLECTION</h1>");
-
-    response.write("<table border ='solid 3px black' align='center'>");
-
-    for (var i=0; i < json.Search.length; i++) {  //Loop and show information
-        response.write("<tr>");
-        response.write(("</td><td><img style='width:70%' src=" + json.Search[i].imageUrl +">"
-        + "</td><td>" + "NAME: " + json.Search[i].name + "</td><td>"
-        + "SET: " + jsonSearch[i].set + "</td>"));
-        response.write("</tr>");
-    };
-    response.write("</table>");
-    response.write("</body></html>");
-
-
-response.end();
-}).listen(8081);
-console.log("Server running at http://127.0.0.1:8081/");
-
-*/
