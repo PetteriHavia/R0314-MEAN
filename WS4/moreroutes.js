4948de1bd09347d673c46a248d4f43ea2c2973f2
PetteriH <petsku15@hotmail.fi>
2020-03-01 17:39:22 +0200

    WS4 stuff

--- WS4/moreroutes.js
+++ WS4/moreroutes.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 app.use(express.static('/public/demoite/'));
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/demosite/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 app.get('/list', function (req, res) {
@@ -15,25 +15,55 @@ app.get('/list', function (req, res) {
 app.get('/jsondata', function(req, res) {
     var data = require('./exampledata2.json');
     res.json(data);
-})
+});
 
 app.get('/details', function (req, res) {
+    //Load the existing data from a file  
     var data = require('./exampledata2.json');
-
-    var result = '<table border="1">';
+    //Parse the results into variable
+    var results = '<table border="1" align="center">';
 
     for(var i=0; i < data.length; i++) {
         results +=
         '<tr>'+
         '<td>' + data[i].Name +'</td>'+
         '<td>' + data[i].Email +'</td>'+
+        '<td>' + data[i].Date +'</td>'+
+        '<td>' + data[i].Company +'</td>'+
         '</tr>';
     }
     res.send(results);
-})
+});
 
 app.get('/add', function(req, res) { 
     res.send('Lets try to add some data to file');
+
+    //Load the existing data from a file    
+    var data = require('./exampledata2.json');
+
+    //Create new JSON object and add it to the existing data variable
+    data.push({
+        "Name": "Petteri Havia",
+        "Company": "Laurea",
+        "Email": "petteri@laurea.fi",
+        "Date": "30/3/2016 \r\n"
+    });
+
+    //Convert the JSON object to a string format
+    var jsonStr = JSON.stringify(data);
+
+    //Write data to file
+    fs.writeFile('exampledata2.json', jsonStr, (err) => {
+        if(err) throw err;
+        console.log('It\'s saved!');
+    });
+
+    res.send("Saved the data to a file. Browse to the /details to see the contents of the file");
+});
+
+app.get('./adduser', function (req, res ) {
+    res.sendFile(__dirname + '/public/adduser.html');
+
 });
 
 app.get('*', function(req, res) {
